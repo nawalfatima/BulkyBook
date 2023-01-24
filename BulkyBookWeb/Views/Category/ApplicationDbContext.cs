833e4ad032509f063a8a09523eab26d33c7740a2
murtajiz110 <murtajiz110@yahoo.com>
2023-01-24 21:48:13 +0500

    _Notifications.cshtml fixed

---
+++ BulkyBookWeb/Views/Category/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
